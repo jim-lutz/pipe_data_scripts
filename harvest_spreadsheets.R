6bb4141c6c3b96b9aa4f3ebb572472d677d026bf
Jim Lutz <jdlutz@hotwaterresearch.net>
2017-08-02 17:20:33 -0700

    parsing even if no test#

--- harvest_spreadsheets.R
+++ harvest_spreadsheets.R
@@ -34,8 +34,13 @@ DT_fn[,path:= str_replace(path, "/home/jiml/HotWaterResearch/projects/Pipe Test 
 # parse file names into fields
 # only parse filenames that begin with DD_
 
+# drop .XLS
+DT_fn[str_detect(filename, "^[0-9][0-9]_"), remaining:=str_replace(filename, ".XLS", "")]
+DT_fn[is.na(remaining),list(filename)]
+
 # nominal pipe diameter
-DT_fn[str_detect(filename, "^[0-9][0-9]_"), nom_diam:=str_extract(filename, "^(..)")]
+DT_fn[!is.na(remaining), nom_diam:=str_extract(remaining, "^(..)")] # get the nominal diameter
+DT_fn[!is.na(nom_diam), remaining := str_replace(remaining, "^(..)_", "")] # remove the nominal diameter
 unique(DT_fn$nom_diam)
 DT_fn[nom_diam=="14", nom_diam:="1/4"]
 DT_fn[nom_diam=="38", nom_diam:="3/8"]
@@ -45,12 +50,12 @@ unique(DT_fn$nom_diam)
 # [1] "1/2" "3/4" NA    "1/4" "3/8"
 
 # T? test?
-DT_fn[str_detect(filename, "^[0-9][0-9]_"), Test:=str_match(filename, "_(T.*).XLS")[,2] ]
+DT_fn[!is.na(remaining), Test:=str_match(remaining, "_(T.*)$")[,2] ] # get the Test
+DT_fn[!is.na(Test), remaining := str_replace(remaining, "_(T.*)$", "")] # remove the nominal diameter
 unique(DT_fn$Test)
 # [1] "T1"   "T2"   NA     "TBAD" "T3"   "T1Z"  "T2Z" 
 
 # rest of stuff between nom_diam & Test
-DT_fn[str_detect(filename, "^[0-9][0-9]_"), remaining:=str_match(filename, "^[0-9][0-9]_(.*)_T.*.XLS")[,2] ]
 sort(unique(DT_fn$remaining))
 
 # material
@@ -63,153 +68,192 @@ DT_fn[material=="CPVC",remaining := str_replace(remaining, "CPVC", "")]
 # CU
 DT_fn[str_detect(remaining, "^CU"), material:="CU"]
 DT_fn[material=="CU",remaining := str_replace(remaining, "CU", "")]
-
-unique(DT_fn[material=="CU",]$remaining)
 sort(unique(DT_fn$material))
 # [1] "CPVC" "CU"   "PEX" 
+DT_fn[,list(n=length(filename)),by=material]
+#    material   n
+# 1:     CPVC  50
+# 2:       CU  93
+# 3:      PEX 169
+# 4:       NA  16
+
+# V velocity?
+DT_fn[str_detect(remaining, "_V[0-9]*$"), velocity:=str_match(remaining, "_V([0-9]*)$")[,2] ]
+DT_fn[!is.na(velocity),remaining := str_replace(remaining, "_V[0-9]*", "")]
+DT_fn[!is.na(velocity),]
+DT_fn[,list(n=length(filename)),by=velocity]
+#    velocity   n
+# 1:       NA 157
+# 2:       12  20
+# 3:        2  35
+# 4:        5  39
+# 5:        8  35
+# 6:        4  38
+# 7:        1   2
+# 8:       77   2
+
+# G GPM?
+DT_fn[str_detect(remaining, "_[0-9]*G$"), GPM:=str_match(remaining, "_([0-9]*)G$")[,2] ]
+DT_fn[!is.na(GPM),remaining := str_replace(remaining, "_[0-9]*G", "")]
+DT_fn[!is.na(GPM),]
+DT_fn[,list(n=length(filename)),by=GPM]
+#    GPM   n
+# 1:  15  18
+# 2:   1  29
+# 3:  25  18
+# 4:   2  24
+# 5:   4  18
+# 6:  NA 201
+# 7: 115   9
+# 8: 230   3
+# 9: 220   3
+# 10: 441   4
+# 11: 880   1
 
 str(DT_fn)
 
 names(DT_fn)
 setcolorder(DT_fn, 
-            c("path", "filename", "filedate", "nom_diam", "Test", "material","remaining")
+            c("path", "filename", "filedate", 
+              "nom_diam", "Test", "material","velocity", "GPM",
+              "remaining")
 )
 
 write.csv(DT_fn, file="list_of_XLS.csv", row.names = FALSE)
 
-
-# libreoffice command and args for converting xls files to csv
-syscommand <- "/opt/libreoffice5.3/program/soffice.bin"
-sysargs <- c("--convert-to",
-             "csv","")
-
-# initialize a data.table and a counter
-DT_all<-data.table()
-n=0
-
-
-# a loop to process all the xls_files
-for( fn in xls_files) { 
-  n = n+1
-  cat(paste0(n," ",fn)) 
-  
-  # put the filename in single quotes
-  sysargs[3] <- paste0("'",fn,"'")
-  
-  # a system call to convert xls files to a csv file in the working directory
-  system2(command = syscommand, args = sysargs)
-  
-  # split path and file name 
-  path <- dirname(fn)
-  filename <- basename(fn)
-  
-  # make the csv file name, could be either XLS or xls
-  csvfn <- str_replace(filename, "(.XLS)|(.xls)", ".csv")
-  
-  # looks like the file structure is not as clean as hoped.
-  # read in the first 2 lines of the csv file as a data.table
-  DT_csv <- data.table(read.csv(csvfn, header = FALSE, nrows = 2 ))
-
-  # add path and filename as fields
-  DT_csv[,path:=path][,filename:=filename]
-  
-  DT_all <- rbind(DT_all, DT_csv)
-  
-  # remove the DT_csv file
-  if (file.exists(csvfn)) file.remove(csvfn)
-
-}
-
-  
-#======
-
-
-
-load(file=paste0(wd_data,"DT.Rdata"))
-
-tables()
-
-# check reading correct Dhw and DhwBu
-names(DT_HPWH)
-DT_HPWH[, list(HPWH.ElecDhw=sum(HPWH.ElecDhw), HPWH.ElecDhwBU=sum(HPWH.ElecDhwBU))]
-names(DT_HPWH01)
-DT_HPWH01[, list(Dhw=sum(Dhw), DhwBU=sum(DhwBU))]
-
-names(DT_ER)
-DT_ER[, list(ER.ElecDhw=sum(ER.ElecDhw), ER.ElecDhwBU=sum(ER.ElecDhwBU))]
-names(DT_ER01)
-DT_ER01[, list(Dhw=sum(Dhw), DhwBU=sum(DhwBU))]
-
-# keep DT_HPWH & DT_ER data.tables
-l_tables <- tables()$NAME
-rm(list = l_tables[grepl("*0[1-5]", l_tables)])
-
-# merge table of electricity use & hot water for HPWH and ER
-DT_EHW <-merge(DT_ER[,list(HoY,JDay,Mon,Day,Hr,sDOWH,nPeople,WH.Total=ER.WH.Total,ER.ElecDhw,ER.ElecDhwBU)],
-               DT_HPWH[,list(HoY,HPWH.ElecDhw, HPWH.ElecDhwBU)], 
-               by = "HoY")
-
-# assume ElecTot and ElecDhwBU are same units, and are separate
-DT_EHW[ , ER.Elec:= ER.ElecDhw + ER.ElecDhwBU]
-DT_EHW[ , HPWH.Elec:= HPWH.ElecDhw + HPWH.ElecDhwBU]
-
-# rearrange data for boxplots of hourly _use
-DT_EHW[ , list(HoY,Hr, GPH=WH.Total, ER=ER.Elec, HPWH=HPWH.Elec)]
-
-# melt into long format
-DT_mEHW <- melt(DT_EHW[ , list(HoY,Hr, GPH=WH.Total, ER=ER.Elec, HPWH=HPWH.Elec)], id.vars = c("HoY","Hr"))
-DT_mEHW[, list(variable=unique(variable))]
-#    variable
-# 1:      GPH
-# 2:       ER
-# 3:     HPWH
-
-# change names
-setnames(DT_mEHW, c("value", "variable"), c("hourly.use", "type.use"))
-
-# boxplots of hourly use by type of use and hour of day
-p <- ggplot(data = DT_mEHW )
-p <- p + geom_boxplot( aes(y = hourly.use, x = as.factor(Hr),
-                           fill = factor(type.use), 
-                           color = factor(type.use),
-                           dodge = type.use),
-                       position = position_dodge(width = .7),
-                       varwidth = TRUE)
-p <- p + scale_fill_manual(values=c("#DDDDFF", "#FFDDDD", "#DDFFDD"),name="use")
-p <- p + scale_color_manual(values=c("#0000FF", "#FF0000", "#00FF00"),name="use")
-p <- p + ggtitle("Hot Water and Electricity Use") + labs(x = "hour", y = "Hourly Use")
-p <- p + scale_x_discrete(breaks=1:24,labels=1:24)
-p
-
-ggsave(filename = paste0(wd_charts,"/Use_by_hour.png"), plot = p)
-
-# now plot one day, high and low
-DT_EHW[, list(GPD=sum(WH.Total)), by = JDay][order(GPD)]
-
-# heavy day, JDay==138
-DT_EHW[JDay==138,]
-DT_heavy <- DT_EHW[JDay==138,list(Hr,GPH=WH.Total,ER=ER.Elec,HPWH=HPWH.Elec)]
-str(DT_heavy)
-p <- ggplot(data = DT_heavy )
-p <- p + geom_line(aes(x=Hr, y=GPH), color="blue", size=2)
-p <- p + geom_line(aes(x=Hr,y=ER), color="red")
-p <- p + geom_line(aes(x=Hr,y=HPWH), color="green")
-p <- p + ggtitle("Hot Water and Electricity Use (Mon, 5/18, 6 people)") + labs(x = "hour", y = "Hourly Use")
-p <- p + scale_x_continuous(breaks=1:24,labels=1:24)
-p
-
-ggsave(filename = paste0(wd_charts,"/HW_elec_heavy.png"), plot = p)
-
-# light day, JDay==15
-DT_EHW[JDay==15,]
-DT_light <- DT_EHW[JDay==15,list(Hr,GPH=WH.Total,ER=ER.Elec,HPWH=HPWH.Elec)]
-str(DT_light)
-p <- ggplot(data = DT_light )
-p <- p + geom_line(aes(x=Hr, y=GPH), color="blue", size=2)
-p <- p + geom_line(aes(x=Hr,y=ER), color="red")
-p <- p + geom_line(aes(x=Hr,y=HPWH), color="green")
-p <- p + ggtitle("Hot Water and Electricity Use (Thurs, 1/15, 2 people)") + labs(x = "hour", y = "Hourly Use")
-p <- p + scale_x_continuous(breaks=1:24,labels=1:24)
-p
-
-ggsave(filename = paste0(wd_charts,"/HW_elec_light.png"), plot = p)
+# 
+# # libreoffice command and args for converting xls files to csv
+# syscommand <- "/opt/libreoffice5.3/program/soffice.bin"
+# sysargs <- c("--convert-to",
+#              "csv","")
+# 
+# # initialize a data.table and a counter
+# DT_all<-data.table()
+# n=0
+# 
+# 
+# # a loop to process all the xls_files
+# for( fn in xls_files) { 
+#   n = n+1
+#   cat(paste0(n," ",fn)) 
+#   
+#   # put the filename in single quotes
+#   sysargs[3] <- paste0("'",fn,"'")
+#   
+#   # a system call to convert xls files to a csv file in the working directory
+#   system2(command = syscommand, args = sysargs)
+#   
+#   # split path and file name 
+#   path <- dirname(fn)
+#   filename <- basename(fn)
+#   
+#   # make the csv file name, could be either XLS or xls
+#   csvfn <- str_replace(filename, "(.XLS)|(.xls)", ".csv")
+#   
+#   # looks like the file structure is not as clean as hoped.
+#   # read in the first 2 lines of the csv file as a data.table
+#   DT_csv <- data.table(read.csv(csvfn, header = FALSE, nrows = 2 ))
+# 
+#   # add path and filename as fields
+#   DT_csv[,path:=path][,filename:=filename]
+#   
+#   DT_all <- rbind(DT_all, DT_csv)
+#   
+#   # remove the DT_csv file
+#   if (file.exists(csvfn)) file.remove(csvfn)
+# 
+# }
+# 
+#   
+# #======
+# 
+# 
+# 
+# load(file=paste0(wd_data,"DT.Rdata"))
+# 
+# tables()
+# 
+# # check reading correct Dhw and DhwBu
+# names(DT_HPWH)
+# DT_HPWH[, list(HPWH.ElecDhw=sum(HPWH.ElecDhw), HPWH.ElecDhwBU=sum(HPWH.ElecDhwBU))]
+# names(DT_HPWH01)
+# DT_HPWH01[, list(Dhw=sum(Dhw), DhwBU=sum(DhwBU))]
+# 
+# names(DT_ER)
+# DT_ER[, list(ER.ElecDhw=sum(ER.ElecDhw), ER.ElecDhwBU=sum(ER.ElecDhwBU))]
+# names(DT_ER01)
+# DT_ER01[, list(Dhw=sum(Dhw), DhwBU=sum(DhwBU))]
+# 
+# # keep DT_HPWH & DT_ER data.tables
+# l_tables <- tables()$NAME
+# rm(list = l_tables[grepl("*0[1-5]", l_tables)])
+# 
+# # merge table of electricity use & hot water for HPWH and ER
+# DT_EHW <-merge(DT_ER[,list(HoY,JDay,Mon,Day,Hr,sDOWH,nPeople,WH.Total=ER.WH.Total,ER.ElecDhw,ER.ElecDhwBU)],
+#                DT_HPWH[,list(HoY,HPWH.ElecDhw, HPWH.ElecDhwBU)], 
+#                by = "HoY")
+# 
+# # assume ElecTot and ElecDhwBU are same units, and are separate
+# DT_EHW[ , ER.Elec:= ER.ElecDhw + ER.ElecDhwBU]
+# DT_EHW[ , HPWH.Elec:= HPWH.ElecDhw + HPWH.ElecDhwBU]
+# 
+# # rearrange data for boxplots of hourly _use
+# DT_EHW[ , list(HoY,Hr, GPH=WH.Total, ER=ER.Elec, HPWH=HPWH.Elec)]
+# 
+# # melt into long format
+# DT_mEHW <- melt(DT_EHW[ , list(HoY,Hr, GPH=WH.Total, ER=ER.Elec, HPWH=HPWH.Elec)], id.vars = c("HoY","Hr"))
+# DT_mEHW[, list(variable=unique(variable))]
+# #    variable
+# # 1:      GPH
+# # 2:       ER
+# # 3:     HPWH
+# 
+# # change names
+# setnames(DT_mEHW, c("value", "variable"), c("hourly.use", "type.use"))
+# 
+# # boxplots of hourly use by type of use and hour of day
+# p <- ggplot(data = DT_mEHW )
+# p <- p + geom_boxplot( aes(y = hourly.use, x = as.factor(Hr),
+#                            fill = factor(type.use), 
+#                            color = factor(type.use),
+#                            dodge = type.use),
+#                        position = position_dodge(width = .7),
+#                        varwidth = TRUE)
+# p <- p + scale_fill_manual(values=c("#DDDDFF", "#FFDDDD", "#DDFFDD"),name="use")
+# p <- p + scale_color_manual(values=c("#0000FF", "#FF0000", "#00FF00"),name="use")
+# p <- p + ggtitle("Hot Water and Electricity Use") + labs(x = "hour", y = "Hourly Use")
+# p <- p + scale_x_discrete(breaks=1:24,labels=1:24)
+# p
+# 
+# ggsave(filename = paste0(wd_charts,"/Use_by_hour.png"), plot = p)
+# 
+# # now plot one day, high and low
+# DT_EHW[, list(GPD=sum(WH.Total)), by = JDay][order(GPD)]
+# 
+# # heavy day, JDay==138
+# DT_EHW[JDay==138,]
+# DT_heavy <- DT_EHW[JDay==138,list(Hr,GPH=WH.Total,ER=ER.Elec,HPWH=HPWH.Elec)]
+# str(DT_heavy)
+# p <- ggplot(data = DT_heavy )
+# p <- p + geom_line(aes(x=Hr, y=GPH), color="blue", size=2)
+# p <- p + geom_line(aes(x=Hr,y=ER), color="red")
+# p <- p + geom_line(aes(x=Hr,y=HPWH), color="green")
+# p <- p + ggtitle("Hot Water and Electricity Use (Mon, 5/18, 6 people)") + labs(x = "hour", y = "Hourly Use")
+# p <- p + scale_x_continuous(breaks=1:24,labels=1:24)
+# p
+# 
+# ggsave(filename = paste0(wd_charts,"/HW_elec_heavy.png"), plot = p)
+# 
+# # light day, JDay==15
+# DT_EHW[JDay==15,]
+# DT_light <- DT_EHW[JDay==15,list(Hr,GPH=WH.Total,ER=ER.Elec,HPWH=HPWH.Elec)]
+# str(DT_light)
+# p <- ggplot(data = DT_light )
+# p <- p + geom_line(aes(x=Hr, y=GPH), color="blue", size=2)
+# p <- p + geom_line(aes(x=Hr,y=ER), color="red")
+# p <- p + geom_line(aes(x=Hr,y=HPWH), color="green")
+# p <- p + ggtitle("Hot Water and Electricity Use (Thurs, 1/15, 2 people)") + labs(x = "hour", y = "Hourly Use")
+# p <- p + scale_x_continuous(breaks=1:24,labels=1:24)
+# p
+# 
+# ggsave(filename = paste0(wd_charts,"/HW_elec_light.png"), plot = p)
